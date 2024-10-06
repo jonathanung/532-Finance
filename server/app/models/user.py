639a319164cfeb295e1c58c60cf00dfa8cfb2d0e
Jonathan Ung <jonathan.keith.ung@gmail.com>
2024-10-05 21:47:51 -0700

    [Backend] Expenses

--- server/app/models/user.py
+++ server/app/models/user.py
@@ -1,4 +1,34 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
+from typing import List
+from datetime import date
+from bson import ObjectId
+
+class ExpenseCreate(BaseModel):
+    expenseType: str = Field(..., alias="expense-type")
+    expenseDate: str  # Keep this as a string
+    expenseTotal: float
+    expenseName: str = Field(..., alias="expense-name")
+
+    class Config:
+        populate_by_name = True
+
+class Expense(ExpenseCreate):
+    id: str = Field(default_factory=lambda: str(ObjectId()), alias="_id")
+
+    class Config:
+        populate_by_name = True
+        json_encoders = {
+            ObjectId: lambda v: str(v)
+        }
+
+class Expense(BaseModel):
+    expense_type: str = Field(..., alias="expense-type")
+    date: date
+    total: float
+    expense_name: str = Field(..., alias="expense-name")
+
+    class Config:
+        populate_by_name = True
 
 class UserCreate(BaseModel):
     first_name: str
@@ -10,6 +40,7 @@ class User(BaseModel):
     first_name: str
     last_name: str
     email: EmailStr
+    expenses: List[Expense] = []
 
 class Token(BaseModel):
     access_token: str
